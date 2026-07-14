bce86a87b46636baa8bbe78674de5f8d89d4eff4
loulilly874-gif <loulilly874@gmail.com>
2026-07-14 03:39:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.get("/test-db", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
